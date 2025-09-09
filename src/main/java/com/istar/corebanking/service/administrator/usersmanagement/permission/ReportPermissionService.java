ccaa7dfd7388ea9b35610e6582c52121751c802c
Pheasa <113016697+Pheasa-Uon@users.noreply.github.com>
2025-09-09 17:19:32 +0700

    Update ReportPermissionService.java

--- src/main/java/com/istar/corebanking/service/administrator/usersmanagement/permission/ReportPermissionService.java
+++ src/main/java/com/istar/corebanking/service/administrator/usersmanagement/permission/ReportPermissionService.java
@@ -1,20 +1,10 @@
 package com.istar.corebanking.service.administrator.usersmanagement.permission;
 
-import com.istar.corebanking.entity.administrator.feature.Feature;
-import com.istar.corebanking.entity.administrator.feature.MainMenu;
 import com.istar.corebanking.entity.administrator.feature.Reports;
 import com.istar.corebanking.entity.administrator.usersmanagement.rolepermission.Role;
-import com.istar.corebanking.entity.administrator.usersmanagement.rolepermission.RoleFeaturePermission;
-import com.istar.corebanking.entity.administrator.usersmanagement.rolepermission.RoleMainMenuPermission;
 import com.istar.corebanking.entity.administrator.usersmanagement.rolepermission.RoleReportPermission;
-import com.istar.corebanking.repository.administrator.feature.FeatureRepository;
-import com.istar.corebanking.repository.administrator.feature.MainMenuRepository;
 import com.istar.corebanking.repository.administrator.feature.ReportRepository;
-import com.istar.corebanking.repository.administrator.usersmanagement.rolepermission.RoleFeaturePermissionRepository;
-import com.istar.corebanking.repository.administrator.usersmanagement.rolepermission.RoleMainMenuPermissionRepository;
 import com.istar.corebanking.repository.administrator.usersmanagement.rolepermission.RoleReportPermissionRepository;
-import com.istar.corebanking.service.administrator.usersmanagement.permission.record.FeaturePermissionFlags;
-import com.istar.corebanking.service.administrator.usersmanagement.permission.record.MenuPermissionFlags;
 import com.istar.corebanking.service.administrator.usersmanagement.permission.record.ReportPermissionFlags;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
